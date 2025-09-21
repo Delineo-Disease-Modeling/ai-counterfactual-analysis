eefa5cce7e0c45a0dfe8e5036b987560077a6040
Liam Perez <lperez19@jh.edu>
2025-09-21 16:14:41 -0400

    added location track + polish

--- tracker/agent_tracker.py
+++ tracker/agent_tracker.py
@@ -18,6 +18,7 @@ def agent_track(data_dir: str):
     total_movements = 0
     total_infections = 0
     total_catches = 0
+    output_file.write("Tracking of agent " + str(track_id) + "\n")
     while (timestamp <= 1440):
         moved_flag = 0
         infected_flag = 0
@@ -61,15 +62,70 @@ def agent_track(data_dir: str):
                 victim_str = "Your agent did not catch any new diseases at time " + str(timestamp) + ".\n"
                 output_file.write(victim_str)
         timestamp += 60
-    last_str = "In total, your agent moved " + str(total_movements) + " times,\n"
-    last_str += "directly infected " + str(total_infections) + " people,\n"
+    last_str = "In total, your agent moved " + str(total_movements) + " time(s),\n"
+    last_str += "directly infected " + str(total_infections) + " person(s),\n"
     last_str += "and caught " + str(total_catches) + " disease(s)."
     output_file.write(last_str)
     output_file.close()
     return 0
 
 def location_track(data_dir: str):
-    print("NOT YET IMPLEMENTED\n")
+    output_file = open(os.path.join(data_dir, "location_track_output.txt"), "w")
+    track_id = int(input("Please enter the ID of the location you want to track (int).\n"))
+    timestamp = 0
+    total_visits = 0
+    total_exits = 0
+    total_infections = 0
+    intro_write_flag = 0
+    with open(os.path.join(data_dir, "location_logs.csv"), mode="r") as intro_file: 
+        intro_table = csv.reader(intro_file)
+        for row in intro_table:
+            if intro_write_flag == 0 and str(row[1]) == str(track_id): 
+                output_file.write("Tracking of " + str(row[2]) + " " + str(row[1]) + "\n")
+                intro_write_flag = 1
+    while (timestamp <= 1440):
+        visit_flag = 0
+        exit_flag = 0
+        catch_flag = 0
+        with open(os.path.join(data_dir, "movement_logs.csv"), mode="r") as move_file:
+            move_table = csv.reader(move_file)
+            for row in move_table:
+                if str(row[0]) == str(timestamp) and str(row[4]) == str(track_id):
+                    total_visits += 1
+                    visit_flag = 1
+                    move_str = "At time " + str(timestamp) + ", agent "
+                    move_str += str(row[1]) + " entered this location.\n"
+                    output_file.write(move_str)
+                if str(row[0]) == str(timestamp) and str(row[2]) == str(track_id): 
+                    total_exits += 1
+                    exit_flag = 1
+                    move_str = "At time " + str(timestamp) + ", agent "
+                    move_str += str(row[1]) + " exited this location.\n"
+                    output_file.write(move_str)
+            if visit_flag == 0: 
+                move_str = "Nobody entered this location at time " + str(timestamp) + ".\n"
+                output_file.write(move_str)
+            if exit_flag == 0:
+                move_str = "Nobody exited this location at time " + str(timestamp) + ".\n"
+                output_file.write(move_str)
+        with open(os.path.join(data_dir, "infection_logs.csv"), mode="r") as infect_file:
+            infect_table = csv.reader(infect_file)
+            for row in infect_table: 
+                if str(row[0]) == str(timestamp) and str(row[11]) == str(track_id):
+                    total_infections += 1
+                    catch_flag = 1
+                    infect_str = "At time " + str(timestamp) + ", agent " + str(row[6]) + " infected "
+                    infect_str += str(row[1]) + " with disease " + str(row[15]) + "at your location.\n"
+                    output_file.write(infect_str)
+            if catch_flag == 0: 
+                infect_str = "Your agent didn't infect anyone at time " + str(timestamp) + ".\n"
+                output_file.write(infect_str)
+        timestamp += 60
+    last_str = "In total, agents went to this location " + str(total_visits) + " time(s),\n"
+    last_str += "left the location " + str(total_exits) + " time(s),\n"
+    last_str += "and infected people here " + str(total_infections) + " time(s)."
+    output_file.write(last_str)
+    output_file.close()
     return 0
 
 def main():
